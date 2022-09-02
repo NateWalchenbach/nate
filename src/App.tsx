7cf7c18a021c380b10c4128ef748dafe64b31109
natewalchenbach <natewalchenbach@Nates-MacBook-Pro.local>
2022-09-01 21:10:03 -0700

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,7 @@ const App: React.FC = () => (
           </Route>
           <Route exact path="/">
             <Redirect to="/home" />
+            {/* TEST */}
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
