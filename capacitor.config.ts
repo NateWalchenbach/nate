3304eae828382b9b397c35f1ae55f46b9708f1da
Nate Walchenbach <natewalchenbach@Nates-MacBook-Pro.local>
2022-09-01 20:19:13 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nate',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
